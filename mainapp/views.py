f62941d3ca9f367d521eb7ea82c2108a91bdea65
Rakib Ali <r.ali@se18.qmul.ac.uk>
2020-12-02 00:36:18 +0000

    Adding data from models to template

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,22 @@
-from django.shortcuts import render
+import datetime as D  # datetime library to get time for setting cookie
+import sys
 
+from django.shortcuts import render
+from django.utils import timezone
+from django.http import HttpResponse, Http404
+from django.template import RequestContext, loader
+
+from django.contrib.auth.hashers import make_password
+from django.db import IntegrityError
+
+
+from .models import News
 def home(request):
-    return render(request,'home.html',)
+    latest=News.objects.first()
+    corresponding=News.objects.all()[1:3]
+    #Fetches the first news article in the db
+    #Then fetches the next 3, setting a limit, skipping the already getched first and up to 3
+    return render(request,'home.html',{
+        'latest':latest,
+        'corresponding':corresponding
+    })
