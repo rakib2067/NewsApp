bc07ef814a933f194b6dc439b2d62d09394ddcd8
Ismael <IsmaelAli79@Outlook.Com>
2020-12-02 01:12:50 +0000

    Registration

--- NewsApp/settings.py
+++ NewsApp/settings.py
@@ -32,12 +32,15 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
+    'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mainapp'
+
 ]
 
 MIDDLEWARE = [
@@ -119,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
